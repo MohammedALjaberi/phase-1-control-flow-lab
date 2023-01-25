bac431c506c413edfdcb6f505d5408ade77c6d70
Mohammed Al-jaberi <aljabri606@gmail.com>
2023-01-25 12:11:14 +0300

    completed

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue <= 400) {
+  return "This is on me !" ; 
+  }
+  else if (someValue >= 400 && someValue <= 2000){
+    return "That will be twenty bucks." ;
+  }
+  else if (someValue > 2000 && someValue <= 2500){
+  return "I will gladly take your thirty bucks." ; 
+  }
+  else 
+  return 'No can do.'; 
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  city === NYC ? 'Ok, sounds good.' : 'No go.' ; 
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(Name){
   // Write your code here!
+  switch(Name){
+    case 'generous' : 
+    return 'Thank you so much.' ;
+    case 'not as generous' : 
+    return 'Thank you.' ; 
+    default : 
+    return 'Bye.' ; 
+  }
 }
+
+function addFive(someNumber) {
+  let result ; 
+  if (someNumber > 0) {
+    result = someNumber + 5;
+  }
+  return result
+}
+console.log(addFive(20));
